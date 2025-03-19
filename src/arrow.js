a8a2a137d3e9261b6a2e105dd4d917ec505e333e
petrickad <petrick.duarte99@gmail.com>
2025-03-19 09:20:16 -0300

    practice arrows

---
+++ src/arrow.js
@@ -0,0 +1,25 @@
+const minhaFuncao = () =>  'Diz olá';
+
+const retornaUmCarro = () => ({
+    modelo: 'Ka',
+    fabricante: 'Ford'
+})
+console.log(minhaFuncao());
+console.log(retornaUmCarro())
+
+const carro = {
+    velocidadeAtual: 40,
+    acelerar: function() {
+        console.log(this);
+        this.velocidadeAtual += 10;
+    },
+    frear: () => {
+        console.log(this);
+        this.velocidade -= 10;
+    }
+}
+
+carro.acelerar();
+carro.frear();
+
+console.log(carro.velocidadeAtual);
